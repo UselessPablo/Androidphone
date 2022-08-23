58d9a20e0d05e1187b71b512c16994fcfdb44f84
pablo a <pabloauterio@gmail.com>
2022-08-23 16:40:15 -0300

    v4.3

--- brillo.js
+++ brillo.js
@@ -4,3 +4,12 @@ container = document.getElementsByClassName('brilloDoc')[0];
 rangeInput.addEventListener("change",function(){
   container.style.filter = "brightness(" + rangeInput.value + "%)";
 });
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
