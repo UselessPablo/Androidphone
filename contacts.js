897ff01227385741aca31acba352df1f99aa94b2
pablo a <pabloauterio@gmail.com>
2022-09-20 17:56:57 -0300

    Se guardan los contactos

--- contacts.js
+++ contacts.js
@@ -77,8 +77,3 @@ function contactoNuevo(contacto) {
     node.style.backgroundColor = "#" + randomColor;
     
 }
-let randomColor = randomColor1();
-function randomColor1() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    return randomColor;
-}
