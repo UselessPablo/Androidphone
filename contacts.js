0413b363623333fa6a51aeda20f974e5d75f4468
pablo a <pabloauterio@gmail.com>
2022-09-21 10:09:11 -0300

    agregando colores a contactos

--- contacts.js
+++ contacts.js
@@ -1,8 +1,9 @@
 
+
+
+
 let contactos = allStorage();
 mostrarContactos(contactos);
-
-
 const btn = document.getElementById('on')
 
 btn.onclick = () => {
@@ -11,7 +12,7 @@ btn.onclick = () => {
     const apellido = document.getElementById('apellido').value;
     const telefono = document.getElementById('telefono').value;
     const correo = document.getElementById('email').value;
-
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     let contacto = {
 
         id: Math.random(1, 100),
@@ -19,14 +20,12 @@ btn.onclick = () => {
         apellido: apellido,
         telefono: telefono,
         correo: correo,
-      
+        color: randomColor,
     }
     localStorage.setItem(contacto.id, JSON.stringify(contacto));
     printed();
     cerrarContact();
-    console.log(contacto);
     contactoNuevo(contacto);
-
 }
 
 function printed() {
@@ -34,30 +33,43 @@ function printed() {
     let data = localStorage.getItem('contacto');
     let devolucionDeContacto = JSON.parse(data);
 }
+
 function cerrarContact() {
     const modalContacts = document.getElementById('modalContacts');
     const btnCancel = document.getElementById('off');
     modalContacts.style.display = "none";
     resetForm();
 }
+
 function resetForm() {
     document.getElementById("formulario").reset();
 }
+
 function allStorage() {
     var contactos = [],
-        keys = Object.keys(localStorage),
+    
+    keys = Object.keys(localStorage),
         i = keys.length;
-    while (i--) {
+    while (i--){
         contactos.push(JSON.parse(localStorage.getItem(keys[i])));
+        
     }
+    contactos = contactos.sort((a, b) => {
+        if (a.nombre < b.nombre) {
+            return -1;
+        }
+    });
     return contactos;
 }
 
 function mostrarContactos(contactos) {
     contactos.forEach(contacto => {
         const node = document.createElement("li");
-        const textnode = document.createTextNode(contacto.nombre + " " + contacto.apellido + '\n' + contacto.telefono + '\n' + contacto.correo);
+        const textnode = document.createTextNode(contacto.nombre + " " + contacto.apellido + '\n' + contacto.telefono + '\n' + contacto.correo );
         node.appendChild(textnode);
+     let color = (contacto.color);
+        node.style.backgroundColor = "#" + color;
+    console.log(color);
         document.getElementById("lista").appendChild(node);
     });
 }
@@ -66,14 +78,13 @@ function openModal() {
     modalContent.style.display = ('block');
 }
 
-
 function contactoNuevo(contacto) {
 
     const node = document.createElement("li");
-    const textnode = document.createTextNode(contacto.nombre + " " + contacto.apellido + '\n' + contacto.telefono + '\n' + contacto.correo );
+    const textnode = document.createTextNode(contacto.nombre + " " + contacto.apellido + '\n' + contacto.telefono + '\n' + contacto.correo);
     node.appendChild(textnode);
     document.getElementById("lista").appendChild(node);
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    node.style.backgroundColor = "#" + randomColor;
-    
+    node.style.backgroundColor = "#" + randomColor ;
+   // localStorage.setItem('colores',JSON.stringify(randomColor));
 }
