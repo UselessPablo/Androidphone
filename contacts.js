d44626ea27513c62830bd9e7ce92ac9655fa81ca
pablo a <pabloauterio@gmail.com>
2022-09-27 17:18:01 -0300

    contactos con llamada

--- contacts.js
+++ contacts.js
@@ -1,29 +1,29 @@
 
 
-
-
-
 let contactos = allStorage();
+
 mostrarContactos(contactos);
+
 const btn = document.getElementById('on')
 
 btn.onclick = () => {
-
+   document.location.reload(true);
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const telefono = document.getElementById('telefono').value;
     const correo = document.getElementById('email').value;
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    let contacto = {
 
-        id: Math.random(1, 100),
+    let contacto = {
+        id: suma() + 1,
         nombre: nombre,
         apellido: apellido,
         telefono: telefono,
         correo: correo,
         color: randomColor,
+        
     }
-    localStorage.setItem(contacto.id, JSON.stringify(contacto));
+    localStorage.setItem(contacto.nombre, JSON.stringify(contacto));
     printed();
     cerrarContact();
     contactoNuevo(contacto);
@@ -31,7 +31,6 @@ btn.onclick = () => {
 }
 
 function printed() {
-
     let data = localStorage.getItem('contacto');
     let devolucionDeContacto = JSON.parse(data);
 }
@@ -49,10 +48,10 @@ function resetForm() {
 
 function allStorage() {
     var contactos = [],
-    keys = Object.keys(localStorage),
+        keys = Object.keys(localStorage),
         i = keys.length;
-    while (i--){
-        contactos.push(JSON.parse(localStorage.getItem(keys[i])));  
+    while (i--) {
+        contactos.push(JSON.parse(localStorage.getItem(keys[i])));
     }
     contactos = contactos.sort((a, b) => {
         if (a.nombre < b.nombre) {
@@ -64,35 +63,67 @@ function allStorage() {
 
 function mostrarContactos(contactos) {
     contactos.forEach(contacto => {
-        const node = document.createElement("li");
-        const textnode = document.createTextNode(contacto.nombre + " " + contacto.apellido + '\n' + contacto.telefono + '\n' + contacto.correo );
+        const node = document.createElement("button");
+        node.setAttribute('class', 'my-template');
+        node.setAttribute('id', contacto.id++)
+        const textnode = document.createTextNode(contacto.nombre + " " + contacto.apellido + '\n' + contacto.telefono + '\n' + contacto.correo);
         node.appendChild(textnode);
-     let color = (contacto.color);
+        let color = (contacto.color);
         node.style.backgroundColor = `#${color}`;
         document.getElementById("lista").appendChild(node);
     });
 }
+
 function openModal() {
     const modalContent = document.getElementById('modalContacts');
     modalContent.style.display = ('block');
 }
 
 function contactoNuevo(contacto) {
-
-    const node = document.createElement("li");
+    const node = document.createElement("button");
     const textnode = document.createTextNode(contacto.nombre + " " + contacto.apellido + '\n' + contacto.telefono + '\n' + contacto.correo);
     node.appendChild(textnode);
     document.getElementById("lista").appendChild(node);
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    node.style.backgroundColor = "#" + randomColor ;
-    
+    node.style.backgroundColor = "#" + randomColor;
+    const btnContact = document.createElement('button');
+    btnContact.setAttribute('class', 'my-template');
+    btnContact.innerHTML;
+    lista.appendChild(btnContact);
 }
-function openPopUp(){
+
+function openPopUp() {
     let popUp = document.getElementById('mensajePop');
     popUp.classList.add('openPopUp');
 }
+
 function closePopup() {
     let popUp = document.getElementById('mensajePop');
     popUp.classList.remove('openPopUp');
-}   
+}
 
+function openModal2() {
+    const modalContent = document.getElementById('modalContent');
+    modalContent.style.display = ('block');
+}
+
+let btnToast = document.querySelectorAll('.my-template');
+console.log(btnToast);
+for (var i = 0; i < btnToast.length; i++) {
+    btnToast[i].addEventListener('click', (contacto) => {
+        Swal.fire({
+            title: 'Llamar a este contacto',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Llamar',
+            denyButtonText: `No, gracias`,
+        }).then((result) => {
+            result.isConfirmed ? window.location.href = "../views/telefono.html" : Swal.fire('Gracias');
+        });
+    });
+};
+
+function suma() {
+    contactos = allStorage();
+    return contactos.length;
+}
--- telefono.js
+++ telefono.js
@@ -1,50 +1,23 @@
-/* DialPad by Anthony Bernard*/
+let contactos = allStorage();
 
-let btns = document.getElementById("btns");
-let showPicked = document.getElementById("showPicked");
-let buttonNumbers = [
-  [1, '<i class="material-icons" style="font-size: 11px">&#xe0d9;</i>'],
-  [2, "ABC"],
-  [3, "DEF"],
-  [4, "GHI"],
-  [5, "JKL"],
-  [6, "MNO"],
-  [7, "PQRS"],
-  [8, "TUV"],
-  [9, "WXYZ"],
-  ["*", ""],
-  [0, ""],
-  ["#", ""]
-];
-let picked = [];
-let deleteNums = document.getElementById("delete");
-let call = document.getElementById("call");
-let end = document.getElementById("end");
+mostrarContactos(contactos);
 
-buttonNumbers.forEach(numbers => {
-  btns.innerHTML += `
-    <button onclick="addNumbers(this.value)" class="btn btn-light m-1" style="width:5rem; hieght:10rem;" type="button" value="${
-      numbers[0]
-    }">${numbers[0]}<p>${numbers[1]}</p></button>`;
-});
-
-function addNumbers(numbers) {
-  picked.push(numbers);
-  showPicked.innerHTML = picked.join("");
+function allStorage() {
+  var contactos = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+  while (i--) {
+    contactos.push(JSON.parse(localStorage.getItem(keys[i])));
+  }
+  return contactos;
 }
 
-deleteNums.addEventListener("click", () => {
-  picked.pop();
-  showPicked.innerHTML = picked.join("");
-  console.log(picked);
-});
-
-call.addEventListener("click", () => {
-  showPicked.innerHTML = `Calling ${picked.join("")}`;
-  picked = [];
-});
-
-end.addEventListener("click", () => {
-  showPicked.innerHTML = "Call Ended";
-  picked = [];
-});
+function mostrarContactos(contactos) {
+    contactos.forEach(contacto => {
+      console.log(contacto.telefono);
+      let node = document.getElementById('output');
+      const textnode = document.createTextNode(contacto.telefono);
+      node.appendChild(textnode);
+    });
+  
+}
